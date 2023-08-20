bbb59457801f814d1a52f1ba3680534bb1506949
christophe jordan noubissi <christophejordannoubissi@christophes-MacBook-Air.local>
2023-08-20 08:10:59 -0400

    second commit

--- app.py
+++ app.py
@@ -1,65 +1,128 @@
-# app.py file
-
-from flask import Flask, redirect, render_template, request,flash
+from flask import Flask, redirect, render_template, request,flash,session, url_for
 from forex_python.converter import CurrencyCodes
+import json
 
 app = Flask(__name__)
 
 import requests
-from currency import is_value_in_array 
+from currency import is_value_in_array
 
-import os
-
-# Generate a secret key (you can use any method to generate a secret key)
-secret_key = os.urandom(24)
 
 # Set the secret key in the Flask app configuration
 app = Flask(__name__)
-app.config['SECRET_KEY'] = secret_key
-
+app.config['SECRET_KEY'] = "jojo237"
 
 
 @app.route('/')
 def home_page():
-    return render_template("index.html")
 
-@app.route('/result')
-def result_page():
     from_currency = request.args.get('Convertfrom')
     to_currency = request.args.get('Convertto')
     amount = request.args.get('Convertamount')
 
-    # Input validation for amount
-    try:
-        amount = float(amount)
-        if amount <= 0 or amount is None :
-            raise ValueError("Amount must be a positive number")
-    except ValueError:
-        # Flash message for invalid amount
-        flash('Please enter a valid amount', 'error')
-        return redirect("/")
+    #  # Store form data in session
+    # session['from_currency'] = from_currency
+    # session['to_currency'] = to_currency
+    # session['amount'] = amount
 
-    url = f'https://api.exchangerate.host/convert?from={from_currency}&to={to_currency}'
-    response = requests.get(url)
-    data = response.json()
+    # # Retrieve form data from session to pre-fill the form fields
+    # from_currency = session.get('from_currency', '')
+    # to_currency = session.get('to_currency', '')
+    # amount = session.get('amount', '')
 
-    if response.status_code == 200 and data.get("success", False):
-        rate_value = data["info"].get("rate")
-        if rate_value is not None:
-            c = CurrencyCodes()
-            result = rate_value * amount
-            rounded_result = round(result, 2)
-            symbols = c.get_symbol(to_currency)
-            # Check if both the amount and the currency are valid
-            return render_template("result.html", rate_value=rate_value, amount=amount, result=rounded_result, symbols=symbols)
+    
+    return render_template("index.html", from_currency="", to_currency= "", amount=amount)
+  
+
+@app.route('/result', methods=['GET', 'POST'])
+def result_page():
+    if request.method == 'POST':
+        from_currency = request.form.get('Convertfrom')
+        to_currency = request.form.get('Convertto')
+        amount = request.form.get('Convertamount')
+
+        has_error = False
+
+        url1="https://api.exchangerate.host/latest"
+        response1=requests.get(url1)
+        data1 = response1.json()
+        # parsed_data = json.loads(data1)
+        # Extract currency codes
+        currency_codes = list(data1['rates'].keys())
+
+
+
+        try:
+            # if len(from_currency) ==0:
+            if from_currency not in currency_codes:
+                raise ValueError("From Currency cannot be empty!")
+                # return render_template('index.html', from_currency=from_currency)
+        except ValueError:
+            flash('Please enter a valid code currency in "converting from" ', 'error')
+            # session["from_currency"]= from_currency
+            has_error = True
+
+        # return render_template('index.html', from_currency=from_currency)
+
+        try:
+            # if len(to_currency) == 0:
+            if to_currency not in currency_codes:
+                raise ValueError("To Currency cannot be empty!")
+                # return render_template('index.html', to_currency=to_currency)
+        except ValueError:
+            flash('Please enter a valid code currency in "converting to" ', 'error')
+            # session["to_currency"]= to_currency
+            has_error = True
+
+        # return render_template('index.html', to_currency=to_currency)
+
+
+
+        try:
+            amount = float(amount)
+            if amount <= 0 or amount is None:
+                raise ValueError("Amount must be a positive number")
+        except ValueError:
+            flash('Please enter a valid amount', 'error')
+            # session["amount"]= amount
+            has_error = True
+
+
+        if has_error:
+            session["amount"]= amount
+            session["from_currency"]= from_currency
+            session["to_currency"]= to_currency
+            # return redirect('/')
+            return render_template('index.html',from_currency=session.get('from_currency'), to_currency= session.get('to_currency'), amount=session.get('amount'))
+
+
+        url = f'https://api.exchangerate.host/convert?from={from_currency}&to={to_currency}'
+        response = requests.get(url)
+        data = response.json()
+
+        if response.status_code == 200 and data.get("success", False):
+            rate_value = data["info"].get("rate")
+            if rate_value is not None:
+                c = CurrencyCodes()
+                result = rate_value * amount
+                rounded_result = round(result, 2)
+                symbols = c.get_symbol(to_currency)
+                return render_template("result.html", rate_value=rate_value, amount=amount, result=rounded_result, symbols=symbols)
+            else:
+                flash('Invalid currency code', 'error')
         else:
-            # Handle case when rate_value is None
-            flash('Invalid currency code', 'error')
-    else:
-        # Check for invalid currency code or API error
-        flash('Invalid currency code or API error', 'error')
+            flash('Invalid currency code or API error', 'error')
+
+    # # Clear the form data from the session
+    # session.pop('from_currency', None)
+    # session.pop('to_currency', None)
+    # session.pop('amount', None)
+
 
     return redirect("/")
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
 
--- test_app.py
+++ test_app.py
@@ -1,58 +1,44 @@
-from unittest import TestCase
+import unittest
+from flask import Flask, session
 from app import app
-# Make Flask errors be real errors, not HTML pages with error info
-app.config['TESTING'] = True
 
-class AppTestCase(TestCase):
+class AppTestCase(unittest.TestCase):
 
-    # This method runs before each test case
     def setUp(self):
         self.app = app.test_client()
         self.app.testing = True
 
-    # This method runs after each test case
-    def tearDown(self):
-        pass
-
-    # Test the home page
     def test_home_page(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Enter your details:', response.data)
+        self.assertIn(b'Convert Amount', response.data)
 
-    # Test the result page with valid data
-    def test_valid_result_page(self):
-        response = self.app.get('/result?Convertfrom=USD&Convertto=EUR&Convertamount=100')
+    def test_result_page_with_valid_data(self):
+        data = {
+            'Convertfrom': 'USD',
+            'Convertto': 'EUR',
+            'Convertamount': '100'
+        }
+        response = self.app.post('/result', data=data)
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'The result is:', response.data)
+        self.assertIn(b'Result', response.data)
+        self.assertIn(b'Conversion Result:', response.data)
 
-    # Test the result page with invalid amount
-    def test_invalid_amount(self):
-        response = self.app.get('/result?Convertfrom=USD&Convertto=EUR&Convertamount=invalid')
-        self.assertEqual(response.status_code, 302)  # Should redirect to home page
-        self.assertIn(b'Please enter a valid amount', response.data)
-
-    # Test the result page with invalid currency code
-    def test_invalid_currency_code(self):
-        response = self.app.get('/result?Convertfrom=XYZ&Convertto=EUR&Convertamount=100')
-        self.assertEqual(response.status_code, 302)  # Should redirect to home page
+    def test_result_page_with_invalid_data(self):
+        data = {
+            'Convertfrom': 'INVALID',
+            'Convertto': 'EUR',
+            'Convertamount': '100'
+        }
+        response = self.app.post('/result', data=data)
+        self.assertEqual(response.status_code, 200)
         self.assertIn(b'Invalid currency code', response.data)
 
-    # Test the result page with invalid amount and currency code
-    def test_invalid_amount_and_currency_code(self):
-        response = self.app.get('/result?Convertfrom=XYZ&Convertto=EUR&Convertamount=invalid')
-        self.assertEqual(response.status_code, 302)  # Should redirect to home page
-        self.assertIn(b'Please enter a valid amount', response.data)
-        self.assertIn(b'Invalid currency code', response.data)
-
-
-    def test_value_in_array(self):
+    def test_is_value_in_array(self):
         arr = [1, 2, 3, 4, 5]
-        value = 3
-        result = is_value_in_array(arr, value)
-        self.assertTrue(result)
-    
-    
+        self.assertTrue(is_value_in_array(arr, 3))
+        self.assertFalse(is_value_in_array(arr, 6))
+
 
 if __name__ == '__main__':
     unittest.main()
